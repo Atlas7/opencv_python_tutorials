3d4e604d5e195b384d5e1f0742eb6089e6294f7f
Johnny <johnnychan0302@gmail.com>
2015-05-19 20:12:15 +0100

    tidy up drawing_functions_in_opencv lesson to use OpenCV 3.x only

--- gui_features_in_opencv/drawing_functions_in_opencv/run_tutorial.py
+++ gui_features_in_opencv/drawing_functions_in_opencv/run_tutorial.py
@@ -23,22 +23,22 @@ print img.shape
 # Draw a diagonal blue line with thickness of 5 px
 # Essentially update img like this:
 # img = cv2.line(image_to_draw_on, start_coordinate, end_coordinate, BGR_tuple, thickness_in_pixels)
-cv2.line(img,(0,0),(511,511),(255,0,0),5)
+img = cv2.line(img,(0,0),(511,511),(255,0,0),5)
 #%%
 # Draw a rectangle with thickness of 3 px
 # Essentially update img like this:
 # img = cv2.rectangle(image_to_draw_on, top_left_coordinate, bottom_right_coordinate, BGR_tuple, thickness_in_pixels)
-cv2.rectangle(img,(384,0),(510,128),(0,255,0),3)
+img = cv2.rectangle(img,(384,0),(510,128),(0,255,0),3)
 #%%
 # Draw a circle with thickness of 1 px (inwards - indicated by the negative sign thickness)
 # Essentially update img like this:
 # img = cv2.circle(image_to_draw_on, center_coordinate, radius, BGR_tuple, thickness_in_pixels)
-cv2.circle(img,(447,63), 63, (0,0,255), -1)
+img = cv2.circle(img,(447,63), 63, (0,0,255), -1)
 #%%
 # Draw an ellipse with thickness of 1 px (inwards - indicated by the negative sign thickness)
 # Essentially update img like this:
 # cv2.ellipse(image_to_draw_on, center_coordinate, (axis_a_length, axis_b_length), angle, startAngle, endAngle, color, thickness)
-cv2.ellipse(img,(256,256),(100,50),0,0,180,255,-1)
+img = cv2.ellipse(img,(256,256),(100,50),0,0,180,255,-1)
 #%%
 # Draw a polyline
 pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
@@ -51,9 +51,9 @@ cv2.polylines(img,[pts],True,(0,255,255))
 # Draw text
 font = cv2.FONT_HERSHEY_SIMPLEX
 # Python 3.x version uses cv2.LINE_AA 
-#cv2.putText(img,'OpenCV',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
+cv2.putText(img,'OpenCV',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
 # Python 2.x version uses cv2.CV_AA
-cv2.putText(img,'OpenCV',(10,500), font, 4,(255,255,255),2,cv2.CV_AA)
+#cv2.putText(img,'OpenCV',(10,500), font, 4,(255,255,255),2,cv2.CV_AA)
 #%%
 # Display image in a window.
 
