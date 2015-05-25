45f5bb7259475846d5dcd7d1f3c3849f75eada26
Johnny <johnnychan0302@gmail.com>
2015-05-25 10:56:46 +0100

    add codes and sample images to lesson smoothing_images

---
+++ image_processing_in_opencv/smoothing_images/smooth_image_with_median_blur.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 25 10:12:03 2015
+Illustrate smoothing of an image with 2D Convolution.
+@author: Johnny
+"""
+#%%
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+#%%
+img_bgr = cv2.imread('opencv_logo_black_background_noisy.png')
+print img_bgr is not None
+#%%
+img_rgb = cv2.cvtColor(img_bgr,cv2.COLOR_BGR2RGB)
+#%%
+median_rgb = cv2.medianBlur(img_rgb,5)
+#%%
+plt.subplot(121),plt.imshow(img_rgb),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(median_rgb),plt.title('cv2.medianBlur')
+plt.xticks([]), plt.yticks([])
+plt.show()
