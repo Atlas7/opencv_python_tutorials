1e6e4728bf35ac755de6247e585703caf8d17f02
Johnny <johnnychan0302@gmail.com>
2015-06-02 21:34:22 +0100

    add working c code and image

---
+++ image_processing_in_opencv/canny_edge_detection/simple_canny.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 02 21:31:17 2015
+
+@author: Johnny
+"""
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('messi.jpg',0)
+edges = cv2.Canny(img,100,200)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+plt.show()
