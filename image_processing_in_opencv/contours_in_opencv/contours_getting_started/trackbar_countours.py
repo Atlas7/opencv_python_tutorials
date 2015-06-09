e2384b341ba245add972ae1351f5aa166b75e0f9
Johnny <johnnychan0302@gmail.com>
2015-06-09 21:24:37 +0100

    generize trackbar code a little

--- image_processing_in_opencv/contours_in_opencv/contours_getting_started/trackbar_countours.py
+++ image_processing_in_opencv/contours_in_opencv/contours_getting_started/trackbar_countours.py
@@ -21,33 +21,37 @@ def nothing(x):
 img = cv2.imread('tom_cruise.jpg')
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+window_name = 'Contour Plotter'
+
 # Initialize window to show
-cv2.namedWindow('contourer')
+cv2.namedWindow(window_name)
 
 # add ON/OFF switch to "contourer"
 switch = '0 : OFF \n1 : ON'
-cv2.createTrackbar(switch, 'contourer', 0, 1, nothing)
+cv2.createTrackbar(switch, window_name, 0, 1, nothing)
 
 # add threshold slidebar to "contourer"
-cv2.createTrackbar('th', 'contourer', 0, 255, nothing)
+th = 'Threshold'
+cv2.createTrackbar(th, window_name, 0, 255, nothing)
 
 # Infinite loop until we hit the escape key on keyboard
 while(1):
     
     out_image = img.copy()
     # get current positions of the trackbars
-    s = cv2.getTrackbarPos(switch, 'contourer')
-    t = cv2.getTrackbarPos('th', 'contourer')
+    s = cv2.getTrackbarPos(switch, window_name)
+    t = cv2.getTrackbarPos(th, window_name)
 
     # re-generate image based on trackbar values
     if s == 0:
         """ do nothing """
-    else:             
+    else: 
+        """ re-plot contours """            
         ret, thresh = cv2.threshold(imgray, t, 255, cv2.THRESH_BINARY)
         image, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)       
         out_image = cv2.drawContours(out_image, contours, -1, (0, 255, 0), 1)
         
-    cv2.imshow("contourer", out_image)
+    cv2.imshow(window_name, out_image)
     k = cv2.waitKey(1) & 0xFF    
     if k == 27:   # hit escape to quit
         break
